9c522f5a5bede8a42ac1c0df1762fc52aba13a69
Rafael <araujoxvr@gmail.com>
2018-02-16 20:31:13 -0200

    Form estoque

--- src/br/com/AsuperArts/BLL/EstoqueBLL.java
+++ src/br/com/AsuperArts/BLL/EstoqueBLL.java
@@ -21,7 +21,7 @@ public class EstoqueBLL {
     Connection con = new Conexao().abrirConexao();
     
     public void cadastrar(EstoqueDTO e){
-     sql = "INSERT INTO tb_estoque(nome, cor, espessura, comprimento, largura, valor_compra, valor_centimetro)VALUES(?,?,?,?,?,?,?);";
+     sql = "INSERT INTO tb_estoque(nome,cor,espessura,comprimento,largura,valorCompra,valorCm)VALUES(?,?,?,?,?,?,?);";
          try {
              PreparedStatement ps = con.prepareStatement(sql);
              ps.setString(1, e.getNome());
@@ -31,7 +31,6 @@ public class EstoqueBLL {
              ps.setInt(5, e.getLargura());
              ps.setInt(6, e.getValorCompra());
              ps.setInt(7, e.getValorCm());
-             
              ps.execute();
          } catch (SQLException ex) {
              Logger.getLogger(EstoqueBLL.class.getName()).log(Level.SEVERE, null, ex);
@@ -47,6 +46,7 @@ public class EstoqueBLL {
              rs = ps.executeQuery();
               while (rs.next()) {
                   EstoqueDTO estoque = new EstoqueDTO();
+                  
                   estoque.setIdEstoque(rs.getInt("id_estoque"));
                   estoque.setNome(rs.getString("nome"));
                   estoque.setCor(rs.getString("cor"));
@@ -55,6 +55,7 @@ public class EstoqueBLL {
                   estoque.setLargura(rs.getInt("largura"));
                   estoque.setValorCompra(rs.getInt("valorCompra"));
                   estoque.setValorCm(rs.getInt("valorCm"));
+                  produtos.add(estoque);
               }
               return produtos;
          } catch (SQLException ex) {
@@ -62,6 +63,7 @@ public class EstoqueBLL {
          }
          return null;
     }
+    
     public void alterar(EstoqueDTO e){
         sql = "UPDATE tb_estoque SET nome=?,cor=?,espessura=?,comprimento=?,largura=?,valorCompra=?,valorCm=? WHERE id_estoque=?;";
          try {
